cc30e0eabee073b69a91295daa8efd71eca621de
adoodevv <88372359+adoodevv@users.noreply.github.com>
2023-08-29 20:14:31 +0000

    Today's js basics

--- basics.js
+++ basics.js
@@ -1,3 +1,10 @@
-console.log(`hello`);
-let x = 3;
-console.log(x);
+// console.log(`hello`);
+let x = 3; // declaring variables
+let y = x + 2;
+
+let first_name = "joe";
+let last_name = "adoo";
+
+let z = x ** 2;
+
+console.log(first_name + " " + last_name);
