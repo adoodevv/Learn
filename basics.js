fde98a14a49a24d3f9e7ddc1a55c66bb099b5bb1
adoodevv <88372359+adoodevv@users.noreply.github.com>
2023-08-30 08:00:04 +0000

    Covered functions

--- basics.js
+++ basics.js
@@ -1,10 +1,19 @@
-// console.log(`hello`);
-let x = 3; // declaring variables
+console.log(`hello`);
+
+// declaring variables
+let x = 3;
 let y = x + 2;
+let z = x ** 2;
 
 let first_name = "joe";
 let last_name = "adoo";
 
-let z = x ** 2;
-
 console.log(first_name + " " + last_name);
+
+// functions
+function multiply(a, b) {
+	let run = 8; // local variable
+    return a * b
+}
+let k = multiply(2, 4);
+console.log(k);
