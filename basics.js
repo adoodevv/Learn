91d45a7cf8327fd8c900f8f3886cc0c06bb634c4
adoodevv <88372359+adoodevv@users.noreply.github.com>
2023-08-30 16:51:32 +0000

    Arrays

--- basics.js
+++ basics.js
@@ -1,6 +1,6 @@
 console.log(`hello`);
 
-// declaring variables
+//declaring variables
 let x = 3;
 let y = x + 2;
 let z = x ** 2;
@@ -11,7 +11,7 @@ let length = first_name.length;
 
 console.log(first_name + " " + last_name);
 
-// functions
+//functions
 function multiply(a, b) {
 	let run = 8; // local variable
     return a * b
@@ -19,7 +19,7 @@ function multiply(a, b) {
 let k = multiply(2, 4);
 console.log(k);
 
-// objects
+//objects
 const car = {
 type : "toyota", 
 color : "black",
@@ -28,7 +28,21 @@ specs : function() {
 }
 };
 
+//literals
 let firstName = "John";
 let lastName = "Doe";
 
 let text = `Welcome ${firstName}, ${lastName}!`;
+
+//arrays
+const cars = ["honda", "toyota", "benz"];
+cars[0] = "ferrari";
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fLen = fruits.length;
+
+let text = "<ul>";
+for (let i = 0; i < fLen; i++) {
+  text += "<li>" + fruits[i] + "</li>";
+}
+text += "</ul>";
