e84a4303b178ce0e7a43a6bcf969ee9e98266f21
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2023-12-10 00:44:14 +0000

    objects

--- index.js
+++ index.js
@@ -7,5 +7,19 @@ console.log(name);
 
 // constants
 const interestRate = 0.3;
-interestRate = 1;
 console.log(interestRate);
+
+//objects
+let zoo = {
+    // curly braces = on=bject literal
+    animal: "dog",
+    keeper: "haqq"
+}
+
+// Dot notation
+zoo.animal = "lion";
+console.log(zoo.animal);
+
+// Bracket notation
+zoo['keeper'] = 'george';
+console.log(zoo.keeper);
