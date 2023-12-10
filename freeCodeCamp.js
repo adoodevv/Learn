bf4b7ff2b1f6bee6bbd7ecfb5ef52ec118ed266f
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2023-12-10 23:51:05 +0000

    function test

---
+++ freeCodeCamp.js
@@ -0,0 +1,9 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+    result += "The " + myAdjective + " " + myNoun + " "+ myVerb + " "  + myAdverb + " to the store";
+    return result;
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"))
+
+//multi-dimensional arrays
