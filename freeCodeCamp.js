666f2953df3c7fb4d2e49c170f39f24f41d3f64b
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2023-12-10 23:57:57 +0000

    multi-dimensional arrays

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -7,3 +7,8 @@ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
 console.log(wordBlanks("dog", "big", "ran", "quickly"))
 
 //multi-dimensional arrays
+var myArray = [[1,2,3], [4,5,6], [7,8,9]];
+
+var myData = myArray[2][1];
+
+console.log(myData);
