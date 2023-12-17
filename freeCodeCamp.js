9d98db21ff111df2a14777e1970db0500691756a
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2023-12-17 18:42:15 +0000

    basic function

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -9,6 +9,13 @@ console.log(wordBlanks("dog", "big", "ran", "quickly"))
 //multi-dimensional arrays
 var myArray = [[1,2,3], [4,5,6], [7,8,9]];
 
-var myData = myArray[2][1];
+var myData = myArray[0][1];
 
 console.log(myData);
+
+//functions
+function ourReusableFunction() {
+    console.log("Heyya, World");
+}
+
+ourReusableFunction();
