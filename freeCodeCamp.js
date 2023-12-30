87afb9f9118f9b3cf00dd98be1efe7a9b0c05bd0
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2023-12-30 12:39:14 +0000

    order logically Else If

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -73,4 +73,16 @@ function testElseIf(val) {
         return "Greater than or equal to 10";
     }
 }
-console.log(testElseIf(12));
+console.log(testElseIf(3));
+
+//Logical order Else If statements
+function orderMyLogic(val) {
+    if (val < 5) {
+        return "Less than 5";
+    } else if (val < 10) {
+        return "Less than 10";
+    } else {
+        return "Greater than or equal to 10";
+    }
+}
+console.log(orderMyLogic(3));
