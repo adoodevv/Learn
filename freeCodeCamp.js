10f52eba991caeeaaca4a0675b662031271e86d4
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2023-12-30 12:56:37 +0000

    Golf game

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -86,3 +86,25 @@ function orderMyLogic(val) {
     }
 }
 console.log(orderMyLogic(3));
+
+//Golf code
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go home"];
+function golfScore(par, strokes) {
+    if (strokes == 5) {
+        return names[0];
+    } else if (strokes <= par - 2) {
+        return names[1];
+    } else if (strokes == par - 1) {
+        return names[2];
+    } else if (strokes == par) {
+        return names[3];
+    } else if (strokes == par + 1) {
+        return names[4];
+    } else if (strokes == par + 2) {
+        return names[5];
+    } else if (strokes >= par + 3) {
+        return names[6];
+    } 
+}
+
+console.log(golfScore(5, 8))
