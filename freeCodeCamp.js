8b6e35815dc18e3f345f0843508b98e48701e076
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2023-12-30 14:12:50 +0000

    switch statements

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -110,3 +110,23 @@ function golfScore(par, strokes) {
 console.log(golfScore(5, 8));
 
 //Switch statements
+function caseInSwitch(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    } 
+    return answer
+} 
+
+console.log(caseInSwitch(2))
