c0c1a5a956212db593e0a0326b8672ca386f4c06
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2023-12-31 04:54:30 +0000

    counting cards

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -137,3 +137,32 @@ function isLess(a, b) {
 } 
 
 console.log(isLess(10, 15));
+
+//counting cards
+var count = 0;
+function cc(card) {
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count ++;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count --;
+            break;
+    }
+    var holdBet = "Hold";
+    if (count > 0) {
+        holdBet = "Bet";
+    } 
+    return count + " " + holdBet;
+}
+
+cc(2); cc("K"); cc(10); cc("K"); cc("A");
+console.log(cc(4));
