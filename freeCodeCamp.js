301a56b2f6d8bbd66fe3ad0e3ac82ca792d941d7
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2024-01-01 16:50:50 +0000

    nesting For loops

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -349,3 +349,16 @@ for (var i = 0; i < myArr.length; i++) {
 
 console.log(myTotal);
 
+//nesting For loops
+function multipyAll(arr) {
+    var product = 1;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            product *= arr[i][j];
+        }
+    }
+    return product;
+}
+
+var multiply = multipyAll([[1,2], [3,4], [5,6,7]]);
+console.log(multiply);
