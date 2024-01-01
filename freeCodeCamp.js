14b1f201920476d282a9ae6434e19f37ed0c68f7
[Jonathan Adoo] <[adoojonathan412@gmail.com]>
2024-01-01 18:22:57 +0000

    generate random whole numbers

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -417,3 +417,12 @@ function randomFraction() {
 }
 
 console.log(randomFraction());
+
+//generate random whole numbers
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+    return Math.floor(Math.random() * 10);
+}
+
+console.log(randomWholeNum());
