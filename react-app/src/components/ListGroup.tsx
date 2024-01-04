f704f60cf5e1f0544dc4aac52e4c2e50ada48021
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-04 02:03:14 +0000

    rendering lists

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,15 @@
 function ListGroup() {
-    return <h1>List Group</h1>;
+  const items = ["New York", "Accra", "Takoradi", "Keta"];
+
+  return (
+    <>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
