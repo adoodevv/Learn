86b02794638fd95e0bef1b4a9a556d7be01a85a1
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-06 22:57:31 +0000

    clearing handle click event

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,27 +1,18 @@
-import { MouseEvent } from "react";
-
 function ListGroup() {
   let items = ["New York", "Accra", "Takoradi", "Keta"];
-  items = [];
+  let selectedIndex = 0;
 
   const getMessage = () => {
     return items.length === 0 ? <p>No items found</p> : null;
   };
-
-  //event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
       <h1>List</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={handleClick}
-          >
+        {items.map((item, index) => (
+          <li className={ selectedIndex === index ? "list-group-item active" : "list-group-item"} key={item} onClick={}>
             {item}
           </li>
         ))}
