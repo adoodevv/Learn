ad9dedab18036ff7faaad557f18687bbb1b44e21
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-06 23:07:25 +0000

    list group highlight behaviour

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
+
 function ListGroup() {
   let items = ["New York", "Accra", "Takoradi", "Keta"];
-  let selectedIndex = 0;
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const getMessage = () => {
     return items.length === 0 ? <p>No items found</p> : null;
@@ -12,7 +14,17 @@ function ListGroup() {
       {getMessage()}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className={ selectedIndex === index ? "list-group-item active" : "list-group-item"} key={item} onClick={}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
