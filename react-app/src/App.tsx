dafbd3c3afa234cd51e78b225e5f4798bc7885dc
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-06 23:28:18 +0000

    passing functions via props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,9 +3,13 @@ import ListGroup from "./components/ListGroup";
 function App() {
   let items = ["New York", "Accra", "Takoradi", "Keta"];
 
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities"/>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
     </div>
   );
 }
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({items, heading}: Props) {
+function ListGroup({items, heading, onSelectItem}: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const getMessage = () => {
@@ -27,6 +29,7 @@ function ListGroup({items, heading}: Props) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
