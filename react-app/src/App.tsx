4e83966f159bae2136b92bb75d8278aa138ca2e3
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-07 02:23:25 +0000

    button component open alert and close alert button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
@@ -9,13 +10,15 @@ function App() {
     console.log(item);
   }
 
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
       <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
-      <Alert>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>
         Hello <span>World</span>
-      </Alert>
-      <Button color = "secondary" onClick={() => console.log("Clicked")}>
+      </Alert>}
+      <Button color = "secondary" onClick={() => setAlertVisibility(true)}>
         My button
       </Button>
     </div>
