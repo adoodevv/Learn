a4232ed485a72fb7fb123854fa23618cce560491
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-07 01:53:44 +0000

    button component excercise

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,5 @@
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -14,6 +15,9 @@ function App() {
       <Alert>
         Hello <span>World</span>
       </Alert>
+      <Button>
+        
+      </Button>
     </div>
   );
 }
---
+++ react-app/src/components/Button.tsx
@@ -0,0 +1,8 @@
+
+const Button = () => {
+  return (
+    <div className="btn btn-primary">Button</div>
+  )
+}
+
+export default Button;
