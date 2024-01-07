a1ac35d637c8ea32b88580060e71a69913b88cf9
Jonathan Darko Adoo <adoojonathan412@gmail.com>
2024-01-07 01:42:55 +0000

    added hildren to support html content

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,3 +1,4 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -10,6 +11,9 @@ function App() {
   return (
     <div>
       <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
