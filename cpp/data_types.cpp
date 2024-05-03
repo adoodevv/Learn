bcb268ee7214ca9d99e124d6c8ad15be44cef462
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-05-03 05:01:51 +0000

    data types

---
+++ cpp/data_types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   char grade = 'A';
+   string name = "Joe";
+   int age = 18;
+   double gpa = 3.5;
+   bool isMale = true;
+
+   cout << "Name: " << name << endl;
+}
