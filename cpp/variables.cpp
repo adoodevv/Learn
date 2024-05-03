deff8049c98a6b2e51f0f0d63894f3eda4599750
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-05-03 03:58:56 +0000

    variables

---
+++ cpp/variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string name = "Joe";
+   int age;
+   age = 18;
+
+   cout << "Hello, " << name << "!" << endl;
+   cout << "You are " << age << " years old." << endl;
+}
