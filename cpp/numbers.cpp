3b8b56201b045705b860bbb5e0dac3c62a3f98bf
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-05-24 08:31:48 +0000

    numbers

---
+++ cpp/numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+   int wnum = 5;
+   double dnum = 5.5;
+   cout << wnum << endl;
+   cout << dnum << endl;
+   cout << 10 % 3 << endl;
+   cout << pow(2, 5) << endl; // 
+   return 0;
+}
