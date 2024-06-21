d0a519e6d10e65a5393950aa6e1cb77cdff6cd22
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-06-21 22:58:16 +0000

    objects in classes

--- cpp/class.cpp
+++ cpp/class.cpp
@@ -7,19 +7,34 @@ class Book
 public:
    string title;
    string author;
+   int pages;
 
-   Book(string aTitle, string aAuthor) // constructor(they can take parameters)
+   Book(string aTitle, string aAuthor, int aPages) // constructor(they can take parameters)
    {
       title = aTitle;
       author = aAuthor;
+      pages = aPages;
    };
+
+   Book(){}; // default constructor
+
+   bool isLong()
+   {
+      if (pages >= 500)
+      {
+         return true;
+      }
+      return false;
+   }
 };
 
 int main()
 {
-   Book book1("The Art of War", "Sun Tzu"); // creating object
+   Book book1("The Art of War", "Sun Tzu", 500); // creating object
 
    cout << book1.author << endl;
+
+   cout << book1.isLong() << endl;
 
    return 0;
 }
