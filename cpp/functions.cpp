eef6d1c60a18e8a934e825f65f17b84c7cd5fb70
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-06-21 19:46:07 +0000

    functions

---
+++ cpp/functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void sayHi(string name)
+{
+   cout << "Hello " << name << endl;
+}
+
+int main()
+{
+   sayHi("Joe");
+   sayHi("Snack girl");
+
+   return 0;
+}
