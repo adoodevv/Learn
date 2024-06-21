d64b8440d12d30211af324f8d9ab3141eb0ce334
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-06-21 17:34:59 +0000

    test ros assignment

---
+++ cpp/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void myfunction();
+
+int main() {
+  myfunction();
+  return 0;
+}
+
+void myfunction() {
+  printf("The function myfunction() has been called   \n");
+}
