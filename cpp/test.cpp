287e3423658e7c9b789db0b7ff2fd93e8291c03c
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-07-10 20:56:06 +0000

    practice contrustors

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
+
 using namespace std;
 
-void myfunction();
+class Human
+{
+private:
+  string description;
 
-int main() {
-  myfunction();
+public:
+  string name;
+  int age;
+
+  Human(string name, int age, string description) : name(name), age(age), description(description) {}
+
+  Human() {} // default contructor
+
+  void introduce()
+  {
+    cout << name << ", you are " << age << " and " << description << endl;
+  }
+  string getDesc()
+  {
+    return description;
+  }
+  void setDesc(string desc)
+  {
+    description = desc;
+  }
+};
+
+int main()
+{
+  Human Joe("Adoo", 19, "an intellect");
+  Joe.introduce();
+
+  cout << Joe.name << endl;
+  cout << Joe.age << endl;
+  Joe.setDesc("some guy");
+  cout << Joe.getDesc() << endl; // accessing description with getter
   return 0;
-}
-
-void myfunction() {
-  printf("The function myfunction() has been called   \n");
 }
